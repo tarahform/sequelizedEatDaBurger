3d245ebb920eee363a07f1949298ef4def1926f0
tarahform <ptarahform@gmail.com>
2018-06-02 13:59:48 -0500

    final

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,38 +1,47 @@
 // think of this as the main file 
-// burger_controller - burger - orm
+// burger_controller - burger 
 
 // controlls all functions for one tables routes
 var express = require('express');
-var burger = require("../models/burger");
+var db = require("../models");
+
 // all api routes
 var router = express.Router();
 
 // routes
 
 // -- get route for the root route using express
-// -- this route needs to connect to the burger.js (models) to select all the data
 router.get("/", function (req, res) {
-    burger.selectAll(function (data) {
-        // this cb `function (data)` becomes cb in burger.js
-        // we are passing this callback function to burger.selectAll
+    db.Burger.findAll().then(function (data) {
         // console.log(data);
         res.render("index", { burgers: data });
     });
 });
 
 router.post("/burgers/insert", function (req, res) {
-    console.log(req.body);
-
-    burger.insertOne(req.body, function (data) {
+    // console.log(req.body);
+    db.Burger.create(
+        {
+            burger_name: req.body.burger_name,
+            devoured: 0
+        }
+    ).then(function (data) {
         res.redirect("/");
     });
 });
 
 router.put("/burgers/:id", function (req, res) {
-    // var updateBurger = { burger_name: "" };
-    burger.updateOne({ devoured: 1 }, req.params.id, function (data) {
-        res.sendStatus(200);
-    });
+    db.Burger.update(
+        {
+            devoured: 1
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }).then(function (data) {
+            res.sendStatus(200);
+        });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
+let db = require("./models");
 var app = express();
 
 // Set the port of our application
@@ -27,8 +27,11 @@ var routes = require("./controllers/burgers_controller");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+// force: true = allows for testing - delete when hosting on heroku
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
   });
-  
+});
+
